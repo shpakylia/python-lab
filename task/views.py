00c93231ce72b4da3fa3e431f1ac1d0debc2086a
shpakylia <shkalylia91@gmail.com>
2018-06-04 22:37:54 +0300

    add view posts

--- task/views.py
+++ task/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Post
+from django.utils import timezone
 from django.shortcuts import render
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'task/post_list.html', {'posts': posts})
+
+
 
 # Create your views here.
