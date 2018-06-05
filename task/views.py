81cc81f748c4a324f11d324a749071d11581ab17
shpakylia <shkalylia91@gmail.com>
2018-06-05 12:52:33 +0300

    add detail posts

--- task/views.py
+++ task/views.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 from .models import Post
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'task/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'task/post_detail.html', {'post': post})
+
 
 
 
